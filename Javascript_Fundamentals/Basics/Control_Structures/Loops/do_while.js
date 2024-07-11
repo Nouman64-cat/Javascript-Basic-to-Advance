4f16e97cb7006b8da0f5ce39e8ea6cc1209fd7db
Nouman64-cat <noumanejazghost@gmail.com>
2024-07-12 02:29:48 +0500

    control structures have studied in detail

---
+++ Javascript_Fundamentals/Basics/Control_Structures/Loops/do_while.js
@@ -0,0 +1,6 @@
+let i = 0;
+
+do {
+    console.log("Number: " + i);
+    i++;
+} while (i < 5);
---
+++ Javascript_Fundamentals/Basics/Control_Structures/Loops/for_each.js
@@ -0,0 +1,9 @@
+let users = [
+    { name: "John", age: 25 },
+    { name: "Jane", age: 28 },
+    { name: "Jack", age: 32 }
+];
+
+users.forEach(user => {
+    console.log(user.name + " is " + user.age + " years old.");
+});
